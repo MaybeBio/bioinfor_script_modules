f8935af8c13fde044f00bb69709a647bee8b2570
Joe Hoye Dow <luxunisgod123@gmail.com>
2025-07-20 13:43:56 +0800

    Update 08_calc_shannon_entropy.py

--- 08_calc_shannon_entropy.py
+++ 08_calc_shannon_entropy.py
@@ -101,3 +101,35 @@ def calc_shannon_entropy(seq:str) -> float:
     sample_entropy = -np.sum([ freq * np.log2(freq) for freq in aa_freq_list ])
     
     return sample_entropy
+
+##############################################################################################################################################################################
+
+def get_shannon_entropy(seq):
+    """
+    计算香农熵，作为序列复杂度的一个指标。
+
+    Args:
+        seq (str): 输入的蛋白质序列字符串
+
+    Returns:
+        float: 香农熵值
+    """
+    # 计算氨基酸频率
+    amino_acid_counts = {}
+    for aa in seq:
+        if aa in amino_acid_counts:
+            amino_acid_counts[aa] += 1
+        else:
+            amino_acid_counts[aa] = 1
+
+    # 计算总氨基酸数
+    total_count = len(seq)
+
+    # 计算香农熵
+    entropy = 0.0
+    for count in amino_acid_counts.values():
+        frequency = count / total_count
+        if frequency > 0:  # 避免对数为零的情况
+            entropy -= frequency * np.log2(frequency)
+
+    return entropy
