698d1888c6be1d815d06ae4eecda1a29404aba31
Joe Hoye Dow <luxunisgod123@gmail.com>
2025-12-03 20:48:25 +0800

    Update 62_Get_PDB_mmCIF_Residue_Name.py

--- 62_Get_PDB_mmCIF_Residue_Name.py
+++ 62_Get_PDB_mmCIF_Residue_Name.py
@@ -1,5 +1,6 @@
 # 获取PDB或者是mmCIF格式文件中残基的具体分子类型
-	
+
+# 1, 
 protein = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K','ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N','GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W','ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M', "UNK": "X"}
 dna = {'DA': 'A', 'DC': 'C', 'DG': 'G', 'DT': 'T'}
 rna = {'A': 'A', 'C': 'C', 'G': 'G', 'U': 'U'}
@@ -15,3 +16,31 @@ def get_type(self):
 
 	else:
 		return "rna"
+
+============================================================================================================================================
+
+# 2,
+# 1. Protein -> CA
+if Polypeptide.is_aa(res, standard=True):
+	if 'CA' in res:
+		rep_atom = res['CA']
+        rtype = "Protein"
+                
+# 2. Nucleic Acid -> C1' (Distinguish DNA/RNA)
+elif "C1'" in res:
+	rep_atom = res["C1'"]
+	rname = res.resname.strip()
+                    
+	if rname in {'DA', 'DC', 'DG', 'DT'}:
+		rtype = "DNA"
+    elif rname in {'A', 'C', 'G', 'U'}:
+        rtype = "RNA"
+    else:
+        rtype = "Nucleic" # Fallback for modified bases
+                
+# 3. Others (Ligands/Ions) -> First Atom
+else:
+	atoms = list(res.get_atoms())
+    if len(atoms) > 0:
+		rep_atom = atoms[0]
+        rtype = "Ligand
