8200f602d7b3b8dfcf33d995f8d5b6f84e809b5e
Joe Hoye Dow <luxunisgod123@gmail.com>
2026-05-30 15:24:47 +0800

    Create 107_Private_Git.sh

---
+++ 107_Private_Git.sh
@@ -0,0 +1,14 @@
+# 共用服务器, 不用--global
+
+# 1. 当前项目当前仓库
+# 设置仅当前项目的 Git 身份
+git config user.name "你的名字"
+git config user.email "你的邮箱"
+
+# 2. 一次性操作
+# 查看
+# git config user.name
+# git config user.email
+# git config --list
+# 再操作在其他地方
+git add . && git -c user.name="你的名字" -c user.email="你的邮箱" commit -m "提交说明" && git push
