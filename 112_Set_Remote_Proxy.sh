71540b0e7eec59b5c038631badea950db94605ae
Joe Hoye Dow <luxunisgod123@gmail.com>
2026-07-21 23:37:19 +0800

    Update 112_Set_Remote_Proxy.sh

--- 112_Set_Remote_Proxy.sh
+++ 112_Set_Remote_Proxy.sh
@@ -1,5 +1,6 @@
 # 远程 Linux 内部程序走本地代理
 # 通过 SSH 远程端口转发，把 Windows 本地的 7892 代理端口映射到远程 Linux 主机上
+# ⚠️ 查看vpn端口、ssh config中设置端口、每次init shell重新export代理
 
 # Windows 本地代理客户端(clash verge、beibei) ，例如http代理端口7892
 # 因为经常变，所以时刻查看一下
